12c04017734b063c6be789a1aa353f210c8782e1
Travis Swicegood <development@domain51.com>
2012-01-09 22:18:19 -0600

    add a test for description

--- maxixe/tests/loader.py
+++ maxixe/tests/loader.py
@@ -9,3 +9,11 @@ class FeatureFinderTestCase(unittest.TestCase):
     def test_loads_name_from_file(self):
         from maxixe.features import basics
         self.assertEqual(basics.name, "Importable features")
+
+    def test_loads_description_from_file(self):
+        from maxixe.features import basics
+        expected = "\n".join([
+            "In order to interact programmatically with tests",
+            "Developers should be able to import features as code",
+        ])
+        self.assertEqual(basics.description, expected)
