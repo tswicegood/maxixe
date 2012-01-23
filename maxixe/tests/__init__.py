ed5041bf1311cf08e02dbd8182efee8735b27ae7
Travis Swicegood <development@domain51.com>
2012-01-22 21:36:56 -0600

    add in a decorators test stub

--- maxixe/tests/__init__.py
+++ maxixe/tests/__init__.py
@@ -1,11 +1,13 @@
 import unittest
 
 import maxixe
+from maxixe.tests import decorators
 from maxixe.tests import loader
 from maxixe.tests import parser
 from maxixe.tests import utils
 
 suite = unittest.TestSuite()
+suite.addTests(unittest.TestLoader().loadTestsFromModule(decorators))
 suite.addTests(unittest.TestLoader().loadTestsFromModule(loader))
 suite.addTests(unittest.TestLoader().loadTestsFromModule(parser))
 suite.addTests(unittest.TestLoader().loadTestsFromModule(utils))
